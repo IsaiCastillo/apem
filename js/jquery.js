e86f0a25b34aa7ce4e676c6dc750a869b5741cdc
IsaiCastillo <enfroisai@gmail.com>
2017-08-03 20:46:35 -0500

    03/agosto/2017

--- js/jquery.js
+++ js/jquery.js
@@ -11,4 +11,40 @@ $(document).ready(function() {
     $('#ceducacion').click(function() {
         $('#contenidos').load('../src/colegios/educacion.html')
     });
+
+     $('#ccienciasj').click(function() {
+        $('#contenidos').load('../src/colegios/cienciasj.html')
+    });
+
+     $('#ccienciasp').click(function() {
+        $('#contenidos').load('../src/colegios/cienciasp.html')
+    });
+
+     $('#cingenieria').click(function() {
+        $('#contenidos').load('../src/colegios/ingenieria.html')
+    });
+
+     $('#ccomunicacion').click(function() {
+        $('#contenidos').load('../src/colegios/comunicacion.html')
+    });
+
+     $('#ccienciat').click(function() {
+        $('#contenidos').load('../src/colegios/cienciat.html')
+    });
+
+     $('#cemprendurismo').click(function() {
+        $('#contenidos').load('../src/colegios/emprendurismo.html')
+    });
+
+     $('#cpsicologia').click(function() {
+        $('#contenidos').load('../src/colegios/psicologia.html')
+    });
+
+     $('#cgastronomia').click(function() {
+        $('#contenidos').load('../src/colegios/gastronomia.html')
+    });
+
+     $('#ccultura').click(function() {
+        $('#contenidos').load('../src/colegios/cultura.html')
+    });
 });
